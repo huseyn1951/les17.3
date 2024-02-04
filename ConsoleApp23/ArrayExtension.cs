8b4a1d53e793f622aa04a523398faa8a3a3f3363
huseyn1951 <sultanlkhuseyn@gmail.com>
2024-02-04 23:37:22 +0400

    array

---
+++ ConsoleApp23/ArrayExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+     public static class ArrayExtension
+    {
+        public static void SumOfElements(this int[] a)
+        {
+            if (a.Length == 1)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(a[0] + a[a.Length - 1]);
+            }
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 3, 5, 6, 7, 12 };
+            arr1.SumOfElements();
+            int[] arr2 = {19,1};
+            arr2.SumOfElements();
+        }
+    }
+}
